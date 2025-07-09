d5be1f56f8dbce4f8e1537fbdb80241ae6b21cb4
Etienne Koekemoer <me@etiennek.com>
2025-07-09 17:57:36 +0200

    favicon support

--- files/src/app/BaseLayout.tsx
+++ files/src/app/BaseLayout.tsx
@@ -18,6 +18,7 @@ const BaseLayout = (props: BaseLayoutProps) => html`<!doctype html>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark">
     <title>${props.title}</title>
+    <link rel="icon" type="image/png" href="/static/img/favicon.png" />
     ${!props.disableHtmx && html`<script src="/static/js/htmx.min.js"></script>`}
     ${!props.disablePico && html`<link rel="stylesheet" href="/static/css/pico.min.css">`}
     ${<Style>{props.style ?? css``}</Style>}
