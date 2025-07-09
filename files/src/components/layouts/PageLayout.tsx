06e483692e4ff50bfd313c6ac1c516db216c7fe7
Etienne Koekemoer <me@etiennek.com>
2025-07-09 14:53:27 +0200

    Small fix

--- files/src/components/layouts/PageLayout.tsx
+++ files/src/components/layouts/PageLayout.tsx
@@ -9,7 +9,7 @@ interface PageProps {
 
 const Layout = (props: PageProps) => html`
 <!doctype html>
-<html lang="{props.lang ?? 'en'}">
+<html lang="${props.lang ?? 'en'}">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
